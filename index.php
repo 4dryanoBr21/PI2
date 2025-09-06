cd28f3a2a0ea6a515784d49f14bd59e5b34d6d20
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 11:07:34 -0300

    auto: 2025-09-06 11:07:34

--- index.php
+++ index.php
@@ -112,3 +112,18 @@
     }
 
 ?>
+
+<?php
+
+  if(isset($_POST['submit'])) {
+
+    include_once("../functions/conexao.php");
+  
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $result = pdo_query($mysqli, "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')");
+
+  }
+?>
