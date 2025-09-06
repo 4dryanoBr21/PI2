9d4d1b593d97e136500e83643c627c86aedad47c
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 16:40:50 -0300

    auto: 2025-09-06 16:40:50

--- functions/registrar.php
+++ functions/registrar.php
@@ -1,19 +1,17 @@
 <?php
-  include 'conexao.php';
-  
-  $nome  = $_POST['nome'];
-  $email = $_POST['email'];
-  $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); // Criptografa a senha
-  
-  $sql = "INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)";
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param("sss", $nome, $email, $senhaHash);
+include 'conexao.php';
 
-  if ($stmt->execute()) {
-      header("Location: ../index.php");
-      
-  } else {
-      echo "Erro: " . $stmt->error;
-  }
+$nome  = $_POST['nome'];
+$email = $_POST['email'];
+$senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); // Criptografa a senha
 
+$sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $nome, $email, $senha);
+
+if ($stmt->execute()) {
+    echo "Usuário cadastrado com sucesso! <a href='index.html'>Voltar</a>";
+} else {
+    echo "Erro: " . $stmt->error;
+}
 ?>
