1b0a9b9155863d35dc0fbae700059b2cdef6e56e
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 16:46:56 -0300

    auto: 2025-09-06 16:46:56

--- functions/registrar.php
+++ functions/registrar.php
@@ -10,7 +10,7 @@ $stmt = $conn->prepare($sql);
 $stmt->bind_param("sss", $nome, $email, $senha);
 
 if ($stmt->execute()) {
-    echo "Usuário cadastrado com sucesso! <a href='index.'>Voltar</a>";
+    echo "Usuário cadastrado com sucesso! <a href='index.php'>Voltar</a>";
 } else {
     echo "Erro: " . $stmt->error;
 }
