53c1a8a297e18f7be4c3bc14b15c4e9ef98367aa
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 11:08:05 -0300

    auto: 2025-09-06 11:08:05

--- index.php
+++ index.php
@@ -43,6 +43,9 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+                <form action="">
+                    
+                </form>
                 <div class="form-floating">
                     <input type="text" class="form-control" id="floatingInputGroup1" placeholder="Username"
                         style="margin-bottom: 18px;">
