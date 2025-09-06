7842be2a7f426b560801c552fec97d81d911ab93
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 14:09:34 -0300

    auto: 2025-09-06 14:09:34

--- index.php
+++ index.php
@@ -67,21 +67,6 @@
                         <button type="submit" class="btn btn-dark">Register</button>
                     </div>
 
-                    <?php
-
-                        if(isset($_POST['submit'])) {
-
-                            include_once("conexao.php");
-  
-                            $nome = $_POST['nome'];
-                            $email = $_POST['email'];
-                            $senha = $_POST['senha'];
-
-                            $result = new mysqli($mysqli, "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')");
-
-                        }
-                    ?>
-
                 </form>
             </div>
         </div>
