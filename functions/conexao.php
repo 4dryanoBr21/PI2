43ed0a6cd58b5ce41fd14c535912b246cd0f086b
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 13:56:16 -0300

    auto: 2025-09-06 13:56:16

--- functions/conexao.php
+++ functions/conexao.php
@@ -6,9 +6,9 @@
     // Create connection
     $conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
 ?> 
