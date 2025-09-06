461c7138790ac620ac49fecabeb2726b1807b604
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 14:51:13 -0300

    auto: 2025-09-06 14:51:13

--- functions/conexao.php
+++ functions/conexao.php
@@ -1,8 +1,12 @@
 <?php
-    $db_host = 'localhost';
-    $db_user = 'root';
-    $dbpassword = '';
-    $dbname = 'projetomi';
-
-    $mysqli = new mysqli($db_host,$db_user,$dbpassword,$dbname);
+    $host = "localhost";
+    $user = "root";   // seu usuário do MySQL
+    $pass = "";       // sua senha do MySQL
+    $db   = "sistema_login";
+    
+    $conn = new mysqli($host, $user, $pass, $db);
+    
+    if ($conn->connect_error) {
+        die("Falha na conexão: " . $conn->connect_error);
+    }
 ?>
