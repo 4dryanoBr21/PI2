83af48a5e6e7fec169fe199f9ea7e9760985ce60
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 11:18:22 -0300

    auto: 2025-09-06 11:18:21

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
             </div>
             <div class="d-grid gap-2">
                 <button class="btn btn-dark" type="submit" style="margin-top: 20px;">Login</button>
+            </div>
         </form>
         <!-- Button Trigger Register Modal -->
         <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
