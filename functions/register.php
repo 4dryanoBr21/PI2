f1a4f31ffbe75adfdc46196325c2e2655bb46cec
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 11:23:34 -0300

    auto: 2025-09-06 11:23:34

--- functions/register.php
+++ functions/register.php
@@ -5,10 +5,10 @@
         include_once("../functions/conexao.php");
   
         $nome = $_POST['cad_nome'];
-            $email = $_POST['cad_email'];
-            $senha = $_POST['cad_senha'];
+        $email = $_POST['cad_email'];
+        $senha = $_POST['cad_senha'];
 
-            $result = pdo_query($mysqli, "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')");
+        $result = pdo_query($mysqli, "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')");
 
-        }
+    }
 ?>
