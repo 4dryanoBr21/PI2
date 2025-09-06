fe285208023bc8b8988f3c7c32bba24daee48cbf
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 11:24:39 -0300

    auto: 2025-09-06 11:24:39

--- functions/login.php
+++ functions/login.php
@@ -1,12 +1,12 @@
 <?php
 
-        include("../projetomi/functions/conexao.php");
+    include("../projetomi/functions/conexao.php");
 
-        if(isset($_POST['email']) || isset($_POST['password'])) {
+    if(isset($_POST['email']) || isset($_POST['password'])) {
 
-            if(strlen($_POST['email']) == 0) {
-            echo "Preencha seu email!";
-            } else if(strlen($_POST['password']) == 0) {
+        if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu email!";
+        } else if(strlen($_POST['password']) == 0) {
             echo "Preencha sua senha!";
             } else {
 
@@ -39,4 +39,4 @@
 
         }
 
-    ?>
+?>
