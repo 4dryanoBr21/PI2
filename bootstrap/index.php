845523bdec6ef3652763ccbafc2b6f7d5a9ea438
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-05 23:23:31 -0300

    auto: 2025-09-05 23:23:31

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -14,9 +14,7 @@
     <div class="container">
         <img src="MI_legenda.png" class="img-fluid" alt="...">
         <form action="">
-            
-        </form>
-        <div class="form-floating mb-3">
+            <div class="form-floating mb-3">
             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
             <label for="floatingInput">Email address</label>
         </div>
@@ -26,6 +24,7 @@
         </div>
         <div class="d-grid gap-2">
             <button class="btn btn-dark" type="button" style="margin-top: 20px;">Login</button>
+        </form>
             <!-- Button Trigger Register Modal -->
             <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                 Register
