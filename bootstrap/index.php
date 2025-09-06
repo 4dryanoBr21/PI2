82c48094f4cc472e15556f41dff1e5221c8c883c
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-05 23:25:15 -0300

    auto: 2025-09-05 23:25:15

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -15,7 +15,7 @@
         <img src="MI_legenda.png" class="img-fluid" alt="...">
         <form action="" method="POST">
             <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                 <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating">
