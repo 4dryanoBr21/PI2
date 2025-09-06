28be46b4f4bc29ea2173740a1e21ea86ee61be76
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 15:03:54 -0300

    auto: 2025-09-06 15:03:54

--- functions/register.php
+++ functions/register.php
@@ -5,10 +5,10 @@
   $email = $_POST['email'];
   $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); // Criptografa a senha
   
-    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+  $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
   $stmt = $conn->prepare($sql);
   $stmt->bind_param("sss", $nome, $email, $senhaHash);
-  
+
   if ($stmt->execute()) {
       echo "Usuário cadastrado com sucesso! <a href='../index.php'>Voltar</a>";
   } else {
