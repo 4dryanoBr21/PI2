44ecc274aff2d2a5eaf91be8011aa4847ff2bb4f
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-05 23:33:06 -0300

    auto: 2025-09-05 23:33:06

--- auto-git.sh
+++ auto-git.sh
@@ -6,7 +6,7 @@ git config user.email "adrianocanto462@gmail.com"
 while true; do
     if [[ -n $(git status --porcelain) ]]; then
         echo "Alterações detectadas, enviando para o repositório..."
-        git add .
+        git add -A
         git commit -m "auto: $(date '+%Y-%m-%d %H:%M:%S')"
         git push origin main
         echo "✅ Commit e push feitos com sucesso!"
