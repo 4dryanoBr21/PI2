4da86404df5abd4443a428355e4d03b4e79bd0b7
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 14:07:47 -0300

    auto: 2025-09-06 14:07:47

--- index.php
+++ index.php
@@ -71,15 +71,15 @@
 
                         if(isset($_POST['submit'])) {
 
-    include_once("conexao.php");
+                        include_once("conexao.php");
   
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+                        $nome = $_POST['nome'];
+                        $email = $_POST['email'];
+                        $senha = $_POST['senha'];
 
-    $result = pdo_query($mysqli, "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')");
+                        $result = pdo_query($mysqli, "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')");
 
-  }
+                        }
 ?>
 
                 </form>
