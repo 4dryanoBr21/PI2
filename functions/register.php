b51bb5f706e511bf4fcf5ad6c969446c18cdc5d0
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-06 11:27:47 -0300

    auto: 2025-09-06 11:27:47

--- functions/register.php
+++ functions/register.php
@@ -2,7 +2,7 @@
 
     if(isset($_POST['submit'])) {
 
-        include_once("../functions/conexao.php");
+        include_once("conexao.php");
   
         $nome = $_POST['cad_nome'];
         $email = $_POST['cad_email'];
