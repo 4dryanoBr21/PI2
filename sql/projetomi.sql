53db8a06c68fe6b4a0b14c568088ca73de7488de
Adriano Canto Junior <adrianocanto462@gmail.com>
2025-10-10 12:30:59 -0300

    sql

--- sql/projetomi.sql
+++ sql/projetomi.sql
@@ -1,85 +1,36 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Tempo de geração: 25/09/2025 às 15:05
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+CREATE DATABASE projetomi;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+USE projetomi;
 
+CREATE TABLE usuario (
+    nome varchar(100),
+    id_usuario int PRIMARY KEY AUTO_INCREMENT,
+    senha varchar(100),
+    email varchar(100)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE reuniao (
+    fk_sala_id_sala int,
+    fk_usuario_id_usuario int,
+    data_hora_solicitacao DATETIME,
+    data_hora_inscricao DATETIME,
+    data_hora_inicio_fala DATETIME
+);
 
---
--- Banco de dados: `projetomi`
---
+CREATE TABLE sala (
+    id_sala int PRIMARY KEY AUTO_INCREMENT,
+    data_hora_criacao DATETIME,
+    nome_sala varchar(100),
+    codigo_da_sala varchar(100),
+    tempo_maximo_fala time
+);
 
--- --------------------------------------------------------
+ALTER TABLE reuniao ADD CONSTRAINT FK_reuniao_1
+    FOREIGN KEY (fk_sala_id_sala)
+    REFERENCES sala (id_sala)
+    ON DELETE SET NULL;
 
---
--- Estrutura para tabela `sala`
---
-
-CREATE TABLE `sala` (
-  `id` int(11) NOT NULL,
-  `nome_sala` varchar(100) DEFAULT NULL,
-  `tempo_fala` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuario`
---
-
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `senha` varchar(100) DEFAULT NULL,
-  `is_logged_in` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `sala`
---
-ALTER TABLE `sala`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `sala`
---
-ALTER TABLE `sala`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
-
---
--- AUTO_INCREMENT de tabela `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE reuniao ADD CONSTRAINT FK_reuniao_2
+    FOREIGN KEY (fk_usuario_id_usuario)
+    REFERENCES usuario (id_usuario)
+    ON DELETE RESTRICT;
