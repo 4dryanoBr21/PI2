04306e73e7d2a70ce885a4e6864ce68706bc7239
Adriano Canto Junior <adrianocanto462@gmail.com>
2025-10-16 19:54:12 -0300

    16/10

---
+++ sql/comandos_uteis.sql
@@ -0,0 +1,10 @@
+SELECT participante.nome_participante FROM participante JOIN sala ON sala.id_sala = participante.fk_sala WHERE sala.id_sala = 2;
+
+UPDATE participante
+SET data_hora_solicitacao = NULL / NOW()
+WHERE id_participante = 2;
+
+SELECT participante.nome_participante
+FROM participante JOIN sala ON sala.id_sala = participante.fk_sala 
+WHERE sala.id_sala = 2
+ORDER BY data_hora_solicitacao ASC;
--- sql/projetomi.sql
+++ sql/projetomi.sql
@@ -1,120 +1,19 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Tempo de geração: 13/10/2025 às 18:22
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+create database projetomi;
+use projetomi;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+create table sala (
 
+  id_sala int PRIMARY KEY AUTO_INCREMENT,
+  nome_sala varchar(100),
+  tempo_de_fala int
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+);
 
---
--- Banco de dados: `projetomi`
---
+create table participante (
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `reuniao`
---
-
-CREATE TABLE `reuniao` (
-  `fk_id_sala` int(11) NOT NULL,
-  `fk_id_usuario` int(11) NOT NULL,
-  `data_hora_solicitacao` datetime DEFAULT NULL,
-  `data_hora_inscricao` datetime DEFAULT NULL,
-  `data_hora_inicio_fala` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `sala`
---
-
-CREATE TABLE `sala` (
-  `id_sala` int(11) NOT NULL,
-  `nome_sala` varchar(100) NOT NULL,
-  `codigo_da_sala` varchar(100) NOT NULL,
-  `data_hora_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
-  `tempo_maximo_fala` time DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuario`
---
-
-CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `senha` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `reuniao`
---
-ALTER TABLE `reuniao`
-  ADD PRIMARY KEY (`fk_id_sala`,`fk_id_usuario`),
-  ADD KEY `fk_id_usuario` (`fk_id_usuario`);
-
---
--- Índices de tabela `sala`
---
-ALTER TABLE `sala`
-  ADD PRIMARY KEY (`id_sala`),
-  ADD UNIQUE KEY `codigo_da_sala` (`codigo_da_sala`);
-
---
--- Índices de tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `sala`
---
-ALTER TABLE `sala`
-  MODIFY `id_sala` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `reuniao`
---
-ALTER TABLE `reuniao`
-  ADD CONSTRAINT `reuniao_ibfk_1` FOREIGN KEY (`fk_id_sala`) REFERENCES `sala` (`id_sala`),
-  ADD CONSTRAINT `reuniao_ibfk_2` FOREIGN KEY (`fk_id_usuario`) REFERENCES `usuario` (`id_usuario`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  id_participante int PRIMARY KEY AUTO_INCREMENT,
+  nome_participante varchar(100),
+  fk_sala int,
+  data_hora_solicitacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (fk_sala) REFERENCES sala(id_sala)
+);
