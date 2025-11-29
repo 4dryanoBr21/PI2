10b91dbb66ebf0d4c9b5a15dcf7c7f731b300c10
Adriano Canto Junior <adrianocanto462@gmail.com>
2025-11-29 09:52:49 -0300

    Add viewport meta tag for responsive design
    Inserted <meta name="viewport" content="width=device-width, initial-scale=1.0"> in the <head> section of all main PHP pages to improve mobile responsiveness and ensure proper scaling on different devices.

--- pages/criador.php
+++ pages/criador.php
@@ -27,6 +27,7 @@ if ($result->num_rows > 0) {
 <html>
 
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
   <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="../style.css">
--- pages/login.php
+++ pages/login.php
@@ -36,6 +36,7 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
 <html>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
     <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../style.css">
